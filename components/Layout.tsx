d3e68387a99770fb9ea6f57ff4e9a78f128d63c3
jaeyeoneej <5ikve@naver.com>
2023-03-02 00:32:43 +0900

    edit: #0 마우스 움직임에 따른 배경 움직임이 번잡스러워 수정 23-03-02

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,5 @@
-import { useRef } from "react";
 import styled from "styled-components";
 import Header from "./Header";
-import useWindowWidth from "./hooks/useWindowWidth";
 
 const Background = styled.div`
   background-color: white;
@@ -14,7 +12,7 @@ const Wrapper = styled.div`
   background-image: url("https://images.unsplash.com/photo-1595113316349-9fa4eb24f884?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2072&q=80");
   background-repeat: no-repeat;
   background-position: center 30%;
-  background-size: 105%;
+  background-size: cover;
   border-radius: 10px;
   box-shadow: 0 20px 30px rgba(0, 0, 0, 0.5);
   max-width: 1600px;
@@ -23,9 +21,9 @@ const Wrapper = styled.div`
   height: 100%;
   width: 100%;
   overflow: hidden;
-  transition: all 2s;
-  @media screen and (max-width: 1400px) {
-    background-size: cover;
+  transition: background 5s ease-in-out;
+  :hover {
+    background-position: center 50%;
   }
 `;
 const Container = styled.div`
@@ -35,34 +33,9 @@ const Container = styled.div`
 `;
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  let windowWidth = useWindowWidth() > 1400;
-  const wrapperRef = useRef<HTMLDivElement>(null);
-  const onMouseMoveHandler = (event: React.MouseEvent<HTMLDivElement>) => {
-    if (wrapperRef.current && windowWidth) {
-      wrapperRef.current.style.transition = "all 0s";
-      wrapperRef.current.style.backgroundPosition =
-        -event.pageX / 40 + "px " + -event.pageY / 40 + "px";
-    }
-  };
-  const onMouseOverHandler = () => {
-    if (wrapperRef.current && !windowWidth) {
-      wrapperRef.current.style.backgroundPosition = "center 50%";
-    }
-  };
-  const onMouseLeaveHandler = (event: React.MouseEvent<HTMLDivElement>) => {
-    if (wrapperRef.current) {
-      wrapperRef.current.style.backgroundPosition = "center 30%";
-      wrapperRef.current.style.transition = "all 5s";
-    }
-  };
   return (
     <Background>
-      <Wrapper
-        ref={wrapperRef}
-        onMouseMove={onMouseMoveHandler}
-        onMouseLeave={onMouseLeaveHandler}
-        onMouseOver={onMouseOverHandler}
-      >
+      <Wrapper>
         <Header />
         <Container>{children}</Container>
       </Wrapper>
